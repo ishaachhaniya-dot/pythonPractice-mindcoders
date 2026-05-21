6f42ff13aebcc5ae741df8d5e347274ec4b7c4e9
Isha Achhaniya <ishaachhaniya@gamil.com>
2026-05-21 17:48:38 +0530

    example of multi dimentional list

--- basic2.py
+++ basic2.py
@@ -176,6 +176,32 @@
 #     print(element)
 
 temps = [[0.0 for h in range(24)] for d in range(31)]
+temp1 = 30
+temp2 = 32
+count = 0
+
+
+for days in temps:
+    if count== 0:
+        days[11]=temp1
+        count = 1
+    else:
+        days[11]= temp2
+        count=0
+
 for element in temps:
     print(element)
-print(len(temps))
+
+total=0.0
+for days in temps:
+    total+=days[11]
+average = total/31
+print("Average temperature at noon:",average)
+
+highest = -100.0 
+for day in temps:
+     for temp in day:
+        if temp > highest:
+            highest = temp
+print("Highest temp:",highest)
+                        
