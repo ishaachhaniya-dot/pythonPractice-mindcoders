266cb5777d60972baee8fca684857e06e1a573e3
Isha Achhaniya <ishaachhaniya@gmail.com>
2026-05-27 17:57:03 +0530

    functions

--- basic2.py
+++ basic2.py
@@ -311,12 +311,108 @@
 #         return number * factorial(number-1)
 # print(factorial(5))
     
-my_tuple = (1, 10, 100)
-t1 = my_tuple + (1000, 10000) 
-t2 = my_tuple * 3 
-print(len(t2)) 
-print(t1)
-print(t2)
-print(10 in my_tuple)
-print(-10 not in my_tuple)
+# my_tuple = (1, 10, 100)
+# t1 = my_tuple + (1000, 10000) 
+# t2 = my_tuple * 3 
+# print(len(t2)) 
+# print(t1)
+# print(t2)
+# print(10 in my_tuple)
+# print(-10 not in my_tuple)
     
+
+# # my_tuple = (10,100,1000)
+# # my_tuple +=(1000,10000)
+# # print(my_tuple)
+
+# tuple_1=(1,2,3)
+# for elem in tuple_1:
+#     print(elem)
+
+# tuple_2=(1,2,3,4)
+# print(5 in tuple_2)
+# print(5 not in tuple_2)
+
+# tuple_3=(1,2,3,4)
+# print(len(tuple_3))
+# print(5 not in tuple_3)
+
+# tuple_4=tuple_1+tuple_2
+# tuple_5=tuple_3*2
+# print(tuple_4)
+# print(tuple_5)
+
+# my_tuple =tuple((1,2,"string"))
+# print(my_tuple)
+# print(type(my_tuple))
+
+# my_list=[1,2,3]
+# print(my_list)
+# print(type(my_list))
+# tup=tuple(my_list)
+# print(tup)
+# print(type(tup))
+
+# var=123
+# t1=(1,)
+# t2=(2,)
+# t3=(3,var)
+
+# t1,t2,t3=t2,t3,t1
+# print(t1,t2,t3)
+# print(type(t1),type(t2),type(t3))
+
+# dictionary={
+# "cat": "chat",
+# "dog": "chien",
+# "horse": "cheval"
+# }
+# phone_numbers={'boss':555, 'suzy':222}
+# empty_dictionary={}
+
+# print(dictionary)
+# print(type(dictionary))
+# print(phone_numbers)
+# print(empty_dictionary)
+# print(type(empty_dictionary))
+
+# print(dictionary['cat'])
+# print(phone_numbers["suzy"])
+
+# print(phone_numbers['president'])
+
+# dictionary = {"cat": "chat", "dog": "chien", "horse": "cheval"}# words = ['cat', 'lion', 'horse']
+# # for word in words:
+# #     if word in dictionary:
+# #         print(word, "->", dictionary[word])
+# #     else:      
+# #         print(word, "is not in dictionary")
+
+# print(dictionary.keys())
+# for key in dictionary.keys():
+#     print(key,"--",dictionary[key])
+
+# for key,value in dictionary.items():
+#     print(key,"--",value)
+# for value in dictionary.values():
+#     print(value)
+
+
+# pol_eng_dictionary = {    
+#                           "zamek": "castle",
+#                           "woda": "water",
+#                           "gleba": "soil" 
+#                                 }
+# copy_dictionary = pol_eng_dictionary.copy()
+# print("copy_dictionary:",copy_dictionary)
+
+# pol_eng_dictionary["zamek"] = "lock" 
+# item = pol_eng_dictionary["zamek"] 
+# print(item) 
+
+phonebook = {}
+print(phonebook)
+phonebook["Adam"] = 3456783958 
+print(phonebook)
+del phonebook["Adam"]
+print(phonebook)
