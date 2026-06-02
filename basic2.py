cc3ed3aace607ba205e4650f172b395b5e401f0e
Isha Achhaniya <ishaachhaniya@gmail.com>
2026-06-02 17:58:26 +0530

    threelevel inheritance

--- basic2.py
+++ basic2.py
@@ -577,12 +577,49 @@
 #         print(issubclass(cls1,cls2),end="\t")
 #     print()
 
-class Super:
-    supvar =1
+# class Super:
+#     supvar =1
 
-class Sub(Super):
-    subvar = 2
+# class Sub(Super):
+#     subvar = 2
 
-obj = Sub()
-print(obj.subvar)
-print(obj.supvar)
+# obj = Sub()
+# print(obj.subvar)
+# print(obj.supvar)
+
+# class Super:
+#     def __init__(self):
+#         self.supVar = 11
+# class Sub(Super):
+#     def __init__(self):
+#         super().__init__()
+#         self.subVar =12
+# obj = Sub()
+# print(obj.subVar)
+# print(obj.supVar)
+
+class level1:
+    variable_1=100
+    def __init__(self):
+        self.var_1=101
+    def fun_1(self):
+        return 102
+    
+class level2(level1):
+    variable_2=200
+    def __init__(self):
+        super().__init__()
+        self.var_2=201
+    def fun_2(self):
+        return 202
+class Level3(level2):
+    variable_3=300
+    def __init__(self):
+        super().__init__()
+        self.var_3=301
+    def fun_3(self):
+        return 302
+obj=Level3()
+print(obj.variable_1, obj.var_1, obj.fun_1())
+print(obj.variable_2, obj.var_2, obj.fun_2())
+print(obj.variable_3, obj.var_3, obj.fun_3())
