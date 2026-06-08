1f5e0fa27bf8a71bfd2b91c3d2093fe0f9d81454
Isha Achhaniya <ishaachhaniya@gamil.com>
2026-06-08 16:48:54 +0530

    class variable example

--- basic2.py
+++ basic2.py
@@ -598,28 +598,40 @@
 # print(obj.subVar)
 # print(obj.supVar)
 
-class level1:
-    variable_1=100
-    def __init__(self):
-        self.var_1=101
-    def fun_1(self):
-        return 102
+# class level1:
+#     variable_1=100
+#     def __init__(self):
+#         self.var_1=101
+#     def fun_1(self):
+#         return 102
     
-class level2(level1):
-    variable_2=200
-    def __init__(self):
-        super().__init__()
-        self.var_2=201
-    def fun_2(self):
-        return 202
-class Level3(level2):
-    variable_3=300
-    def __init__(self):
-        super().__init__()
-        self.var_3=301
-    def fun_3(self):
-        return 302
-obj=Level3()
-print(obj.variable_1, obj.var_1, obj.fun_1())
-print(obj.variable_2, obj.var_2, obj.fun_2())
-print(obj.variable_3, obj.var_3, obj.fun_3())
+# class level2(level1):
+#     variable_2=200
+#     def __init__(self):
+#         super().__init__()
+#         self.var_2=201
+#     def fun_2(self):
+#         return 202
+# class Level3(level2):
+#     variable_3=300
+#     def __init__(self):
+#         super().__init__()
+#         self.var_3=301
+#     def fun_3(self):
+#         return 302
+# obj=Level3()
+# print(obj.variable_1, obj.var_1, obj.fun_1())
+# print(obj.variable_2, obj.var_2, obj.fun_2())
+# print(obj.variable_3, obj.var_3, obj.fun_3())
+
+
+class Exampleclasss:
+    def __init__(self,val=1):
+        if val%2!=0:
+            self.a=1
+        else:
+            self.b=1
+example_object=Exampleclasss(8)
+
+# print(example_object.a)
+print(example_object.b)
