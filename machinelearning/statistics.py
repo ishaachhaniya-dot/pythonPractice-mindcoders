8c400c4e1b48137494ec5234c19211c32e54caf6
Isha Achhaniya <ishaachhaniya@gamil.com>
2026-06-17 17:55:15 +0530

    python:sns boxplot

--- machinelearning/statistics.py
+++ machinelearning/statistics.py
@@ -4,10 +4,10 @@ import pandas as pd
 import matplotlib.pyplot as plt
 from scipy import stats
 
-#employee salaries(in thousand)
+'''employee salaries(in thousand)'''
 salaries=[22,28,35,42,38,55,48,60,72,85,30,45,52,65,28,34,41,58,75,90]
 
-#central tendency-where is the 'centre' of data
+'''central tendency-where is the 'centre' of data'''
 mean =np.mean(salaries) #average
 median=np.median(salaries) #middle value when sorted
 mode=stats.mode(salaries,keepdims=True).mode[0] #most frequent
@@ -15,3 +15,13 @@ mode=stats.mode(salaries,keepdims=True).mode[0] #most frequent
 print(f'mean (average):Rs.{mean:.1f}k')
 print(f'median  (middle value):Rs.{median}k')
 print(f'mode  (most common): Rs.{mode}k')
+
+'''spread--how varied is data'''
+std=np.std(salaries)
+var=np.var(salaries)
+rng=max(salaries)-min(salaries)
+q1=np.percentile(salaries,25)
+q3=np.percentile(salaries,75)
+iqr= q3-q1
+print(f'std daviation:{std:.2f}K  (most important spread measure)')
+print(f'IQR:{iqr}K   (Q1={q1}, Q3={q3})')
