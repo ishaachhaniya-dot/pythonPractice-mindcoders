731f1da15c826eaf58ebe351febd4ea9d65fe551
Isha Achhaniya <ishaachhaniya@gmail.com>
2026-06-29 12:55:27 +0530

    mini csv project

--- miniproject.py
+++ miniproject.py
@@ -23,7 +23,8 @@ subjects = [
     'Science',
     'English',
     'Computer',
-    'History'
+    'History',
+    'study_hours'
 ]
 df['total']=df[subjects].sum(axis=1)
 print([['name','total']])
@@ -50,5 +51,77 @@ df['Grade'] = df['Average'].apply(get_grade)
 print(df[['Name', 'Grade']])
 
 #calculate ranks
+df['Rank'] = df['total'].rank(ascending=False).astype(int)
+print(df[['Name','Rank']])
+
+#calculate class average
+class_average = (df['Average'].mean())
+print("Class Average:",round(class_average,2))
+
+#topper of the class
+Topper = df.loc[df['total'].idxmax()]#idxmax give us max value,#.loc gives full row
+print("TOPPER:",Topper['Name'])
+
+#failure rate
+failed = len(df[df["Grade"]=="F"])
+
+total_students = len(df)
+
+failure_rate = (failed/total_students)*100
+
+print("Failure Rate :",failure_rate)
+
+#city wise
+city_wise = df.groupby('City')['Average'].mean().astype(int)
+print("City Wise Average:",city_wise)
+
+#Bar graph on basis of city avg
+plt.figure(figsize=(8,5))
+city_wise.plot(kind='bar')
+plt.title("City Wise Average Marks"); plt.ylabel("Average Marks"); plt.tight_layout(); plt.show()
+
+#pie chart on basis of grade distribution
+df["Grade"].value_counts().plot(
+    kind="pie",autopct='%1.0f%%')
+plt.title("Grade Distribution")
+plt.ylabel("")
+plt.tight_layout()
+plt.show()
+
+#scatter plot
+plt.figure(figsize=(8,5))
+
+plt.scatter(df['study_hours'],df['Average'], s=100,alpha=0.8)
+plt.xlabel("Study Hours")
+plt.ylabel("Average Marks")
+plt.title("Study Hours vs Marks")
+plt.show()
+
+#correaltion
+plt.figure(figsize=(8,6))
+
+sns.heatmap(df[subjects].corr(),annot=True)
+plt.title("Subject Correlation Heatmap")
+plt.show()
 
 
+report = pd.DataFrame({
+    "Summary": [
+        "Class Average",
+        "Topper",
+        "Failure Rate"
+    ],
+    "Value": [
+        class_average,
+        Topper["Name"],
+        str(failure_rate) + "%"
+    ]
+})
+
+report.to_csv(
+    "report.csv",
+    index=False
+)
+
+print("\nFiles Saved:")
+print("student.csv")
